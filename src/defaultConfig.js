49761a93b0a4cb6ec410b7cdec12771277c672ee
Au <phamvuau@gmail.com>
2020-04-22 23:45:01 +0700

    simplify command structure

---
+++ src/defaultConfig.js
@@ -0,0 +1,55 @@
+export default {
+  undo: {
+    className: 'fas fa-undo'
+  },
+  redo: {
+    className: 'fas fa-redo'
+  },
+  bold: {
+    className: 'fas fa-bold',
+    shortcuts: ['Mod-b', 'Mod-B']
+  },
+  italic: {
+    className: 'fas fa-italic',
+    shortcuts: ['Mod-i', 'Mod-I']
+  },
+  strikethrough: {
+    className: 'fas fa-strikethrough',
+    shortcuts: ['Mod-s', 'Mod-S']
+  },
+  h1: {
+    text: 'H1',
+    className: 'avenir'
+  },
+  h2: {
+    text: 'H2',
+    className: 'avenir'
+  },
+  h3: {
+    text: 'H3'
+  },
+  h4: {
+    text: 'H4',
+    className: 'avenir'
+  },
+  orderedList: {
+    className: 'fas fa-list-ol'
+  },
+  unorderedList: {
+    className: 'fas fa-list-ul'
+  },
+  indent: {
+    className: 'fas fa-indent',
+    shortcuts: ['Tab']
+  },
+  outdent: {
+    className: 'fas fa-outdent',
+    shortcuts: ['Shift-Tab']
+  },
+  quote: {
+    className: 'fas fa-quote-left'
+  },
+  code: {
+    className: 'fas fa-code'
+  }
+}
